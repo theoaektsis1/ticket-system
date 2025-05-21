e907dfedecf8646101206619edb32caa56877cf0
theoaektsis1 <104593995+theoaektsis1@users.noreply.github.com>
2025-05-21 16:49:53 +0200

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747838993611

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__28acc241._.js",
+  "static/chunks/[root-of-the-server]__dc687cf0._.css"
+],
+    source: "dynamic"
+});
